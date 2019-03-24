caa4d8c64b1cb3deb18f08598a1102472319132c
angelsol299 <angelsol299@hotmail.com>
2019-03-24 20:57:17 +0100

    adding router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router";
 import "./App.css";
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import uuid from "uuid";
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Take out the trash",
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Dinner with GF",
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Study coding",
         completed: false
       }
@@ -44,19 +46,31 @@ class App extends Component {
     });
   };
 
+  //Add Todo
+  addTodo = title => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <AddTodo addTodo={this.addTodo} />
+            <Todos
+              todos={this.state.todos}
+              markComplete={this.markComplete}
+              delTodo={this.delTodo}
+            />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
