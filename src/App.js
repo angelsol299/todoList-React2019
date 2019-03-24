ad97211bec71dad9f5ca910b3c1dd0cb619ecb82
angelsol299 <angelsol299@hotmail.com>
2019-03-24 21:16:19 +0100

    axios delete

--- src/App.js
+++ src/App.js
@@ -35,9 +35,11 @@ class App extends Component {
 
   //Delete Todo
   delTodo = id => {
-    this.setState({
-      todos: [...this.state.todos.filter(todo => todo.id !== id)]
-    });
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
   };
 
   //Add Todo
