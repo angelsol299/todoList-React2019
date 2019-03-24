5a05fc3a1630eb6bf80db9947704626506c72434
angelsol299 <angelsol299@hotmail.com>
2019-03-24 21:15:00 +0100

    axios post ang get

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
-import uuid from "uuid";
+//import uuid from "uuid";
+import axios from "axios";
 
 class App extends Component {
   state = {
     todos: []
   };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => {
+        this.setState({ todos: res.data });
+      });
+  }
 
   //Toggle complete
   markComplete = id => {
@@ -33,12 +42,12 @@ class App extends Component {
 
   //Add Todo
   addTodo = title => {
-    const newTodo = {
-      id: uuid.v4(),
-      title: title,
-      completed: false
-    };
-    this.setState({ todos: [...this.state.todos, newTodo] });
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        complete: false
+      })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
   };
 
   render() {
