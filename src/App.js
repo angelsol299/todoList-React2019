2d973b44be24bb6b965cdcb8e78f9817bb1ae48a
angelsol299 <angelsol299@hotmail.com>
2019-03-24 18:50:54 +0100

    eraising todo tasks

--- src/App.js
+++ src/App.js
@@ -23,14 +23,33 @@ class App extends Component {
     ]
   };
 
+  //Toggle complete
   markComplete = id => {
-    console.log(id);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  //Delete Todo
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
