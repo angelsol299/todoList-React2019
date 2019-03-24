0036ab1a1dfdc1c48d17880cfc82864ef2257546
angelsol299 <angelsol299@hotmail.com>
2019-03-24 21:08:00 +0100

    styling links

---
+++ src/components/layout/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <header style={headerStyle}>
+        <h1>Todo List</h1>
+        <Link style={linkStyle} to="/">
+          {" "}
+          Home{" "}
+        </Link>{" "}
+        |
+        <Link style={linkStyle} to="/about">
+          {" "}
+          About{" "}
+        </Link>
+      </header>
+    </div>
+  );
+};
+
+const headerStyle = {
+  background: "#333",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px"
+};
+
+const linkStyle = {
+  color: "white",
+  textDecoration: "none"
+};
+
+export default Header;
