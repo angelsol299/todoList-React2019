17c3f2f4fb7909a669d567e04c47d5db52c805b8
angelsol299 <angelsol299@hotmail.com>
2019-03-24 17:54:35 +0100

    first components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with GF",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Study coding",
+        completed: false
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
+}
+
+export default App;
